97ab0393dabe21d5c37849289af265fa482e708b
rohinbisht@gmail.com <rohinbisht@gmail.com>
2017-11-29 00:29:44 +0530

    update

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ import App from './app.jsx'
 console.log('setup');
 
 ReactDom.render(<App/>,document.getElementById('mainContainer'))
+
+/*"es6-promise": "^4.1.1",
+    "isomorphic-fetch": "^2.2.1",*/
